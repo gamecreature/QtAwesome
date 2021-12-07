b2ad2e287b65c2ff5201775f313915c7149b669c
Martin Delille <martin@delille.org>
2021-12-07 11:41:00 +0100

    Fix warnings

--- QtAwesome/QtAwesome.h
+++ QtAwesome/QtAwesome.h
@@ -826,7 +826,7 @@ Q_OBJECT
 
 public:
 
-    explicit QtAwesome(QObject *parent = 0);
+    explicit QtAwesome(QObject *parent = nullptr);
     virtual ~QtAwesome();
 
     void init( const QString& fontname );
